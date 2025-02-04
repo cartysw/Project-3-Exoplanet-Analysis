2df766e523b0229af7e2ff913a1231c022c8004d
cartysw <cartysw@gmail.com>
2025-02-03 18:44:01 -0600

    Delete exoplanets.js

--- exoplanets.js
+++
@@ -1,4 +0,0 @@
-// Visualization Plans/Ideas:
-// 3D Scatter Plot (3d representation of planets discovered, ordered by which facilities discovered them)
-// Standard Scatter Plot (Show Discoveries Over Time)
-// Space Kit library usage (Each of us picks a planet, we can add those into the list of planets you can view)
